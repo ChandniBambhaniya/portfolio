6486f66a6baac626ef22b96730cc56ab7545cc82
ChandniBambhaniya <chandnisorathiya1358@gmail.com>
2025-04-17 17:46:57 +0530

    Add scroll margin to Contact and Experience sections for improved layout

--- src/componets/Contact/contact.jsx
+++ src/componets/Contact/contact.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles1 from "../Experience/experience.module.css";
 import styles from "./contact.module.css";
 import mailImg from "../../../assets/media/mail.png";
 import linkedInImg from "../../../assets/media/linkedIn.png";
@@ -6,7 +7,7 @@ import gitImg from "../../../assets/media/git.png";
 
 export const Contact = () => {
   return (
-    <section id="contact" className={styles.container}>
+    <section id="contact" className={[styles.container, styles1.scrollSection]}>
       <div className={styles.leftPart}>
         <h1>Your Move</h1>
         <h4>Got a vision? I'm all ears. Let's bring it to life.</h4>
