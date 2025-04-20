8c66c5815e882dadf12633f22b9d5dbe7d214896
ChandniBambhaniya <chandnisorathiya1358@gmail.com>
2025-04-20 11:54:12 +0530

    Refactor Navbar component for improved menu item handling and styling

--- src/componets/Navbar/navbar.jsx
+++ src/componets/Navbar/navbar.jsx
@@ -4,24 +4,29 @@ import {getImageURL} from "../../utils";
 import menuIcon from "../../../assets/nav/menuOpen.png";
 import menuCloseIcon from "../../../assets/nav/menuClose.png";
 export const Navbar = () => {
+
   const [isMenuOpen, setMenuOpen] = useState(false);
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <nav className={styles.navbar}>
       <a className={styles.title} href="/">Portfolio</a>
       <div className={styles.menu}>
         <img className={styles.menuBtn} src={isMenuOpen? menuCloseIcon : menuIcon } alt="menu-button"  onClick={() => setMenuOpen(!isMenuOpen)} />
-        <ul className={`${styles.menuItems} ${isMenuOpen && styles.menuOpen}`} onClick={() => setMenuOpen(false)}>
-          <li onClick={() => setMenuOpen(false)}>
-            <a href="#about">About</a>
+        <ul className={`${styles.menuItems} ${isMenuOpen && styles.menuOpen}`} >
+          <li>
+            <a href="#about" onClick={closeMenu}>About</a>
           </li>
-          <li onClick={() => setMenuOpen(false)}> 
-            <a href="#experience">Experience</a>
+          <li> 
+            <a href="#experience" onClick={closeMenu}>Experience</a>
           </li>
-          <li onClick={() => setMenuOpen(false)}>
-            <a href="#projects">Projects</a>
+          <li>
+            <a href="#projects" onClick={closeMenu}>Projects</a>
           </li>
-          <li  onClick={() => setMenuOpen(false)}>
-            <a href="#contact1">Contact</a>
+          <li>
+            <a href="#contact1" onClick={closeMenu}>Contact</a>
           </li>
         </ul>
       </div>
